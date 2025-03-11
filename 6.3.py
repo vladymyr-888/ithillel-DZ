b59ab32b8381b8553bd5f233c4f631d649b124d1
48731 <vladcasings@gmail.com>
2025-03-11 11:25:40 +0200

    home work 6.3

--- 6.3.py
+++ 6.3.py
@@ -1,7 +1,7 @@
 number = int(input('Enter number'))
 while number > 9:
      product = 1
-     for numbers in str(number):
-         product *= int(numbers)
+     for digit in str(number):
+         product *= int(digit)
      number = product
 print(number)
