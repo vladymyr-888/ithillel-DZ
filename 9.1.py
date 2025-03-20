6aafd41b7b4f63f3a2d9a3201e2bc40278f7fb23
48731 <vladcasings@gmail.com>
2025-03-20 10:06:33 +0200

    home work 9.1

--- 9.1.py
+++ 9.1.py
@@ -1,6 +1,9 @@
 def popular_words (text, words):
     word_count = {}
-    text = text.lower()
+    text = text.lower().split()
     for word in words:
        word_count[word]  =  text.count(word)
-       return word_count
+    return word_count
+print(popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']))
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }
+print('OK')
